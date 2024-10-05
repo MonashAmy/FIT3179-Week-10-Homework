55fda714faea150c295b7f17419df55cc6e640ab
MonashAmy <116072874+MonashAmy@users.noreply.github.com>
2024-10-05 20:21:01 +1000

    Updated files

---
+++ graph.js
@@ -0,0 +1,7 @@
+var vg_1 = "graph1.vg.json";
+vegaEmbed("#World_Population_Choropleth", vg_1).then(function(result) {
+}).catch(console.error);
+
+var vg_1 = "graph2.vg.json";
+vegaEmbed("#World_Population_Bar_Chart", vg_1).then(function(result) {
+}).catch(console.error);
